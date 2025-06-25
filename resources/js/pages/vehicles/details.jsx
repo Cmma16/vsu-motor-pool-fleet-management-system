068c8157d63ecae4843c8dcd6e8a8da7be0af60d
Carlos Miguel Advincula <1cmadvincula@gmail.com>
2025-06-25 17:53:31 +0800

    commit

--- resources/js/pages/vehicles/details.jsx
+++ resources/js/pages/vehicles/details.jsx
@@ -1,5 +1,3 @@
-import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
-
 import { Head, router } from '@inertiajs/react';
 import React from 'react';
 
@@ -180,9 +178,6 @@ export default function details({ vehicle, odometer_reading, nextMaintenance, la
                         </Card>
                     </div>
                 </div>
-            </div>
-            <div className="border-sidebar-border/70 dark:border-sidebar-border relative min-h-[100vh] flex-1 overflow-hidden rounded-xl border md:min-h-min">
-                <PlaceholderPattern className="absolute inset-0 size-full stroke-neutral-900/20 dark:stroke-neutral-100/20" />
             </div>
         </AppLayout>
     );
