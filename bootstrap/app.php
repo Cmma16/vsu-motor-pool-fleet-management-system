bdede763af48acfc33aa29eb03c4b081235b15c8
Carlos Miguel Advincula <1cmadvincula@gmail.com>
2025-06-26 19:26:24 +0800

    commit

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 use Illuminate\Http\Middleware\TrustProxies;
+use Illuminate\Http\Request;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
