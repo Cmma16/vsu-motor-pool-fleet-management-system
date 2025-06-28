d2e110660631106d5421ddc381cd981668b22d87
Carlos Miguel Advincula <1cmadvincula@gmail.com>
2025-06-28 16:14:17 +0800

    added a loading screen

--- resources/js/layouts/app-layout.jsx
+++ resources/js/layouts/app-layout.jsx
@@ -1,7 +1,27 @@
+import AppLoader from '@/components/display/app-loader';
 import AppLayoutTemplate from '@/layouts/app/app-sidebar-layout';
+import { Inertia } from '@inertiajs/inertia';
+import { useEffect, useState } from 'react';
 
-export default ({ children, breadcrumbs, pageDetails, showQuickActions = true, ...props }) => (
-    <AppLayoutTemplate breadcrumbs={breadcrumbs} pageDetails={pageDetails} showQuickActions={showQuickActions} {...props}>
-        {children}
-    </AppLayoutTemplate>
-);
+export default function AppLayout({ children, breadcrumbs, pageDetails, showQuickActions = true, ...props }) {
+    const [isLoading, setIsLoading] = useState(false);
+
+    useEffect(() => {
+        Inertia.on('start', () => setIsLoading(true));
+        Inertia.on('finish', () => setIsLoading(false));
+    }, []);
+
+    return (
+        <>
+            {isLoading && (
+                <div className="fixed inset-0 z-50 flex items-center justify-center bg-white/80">
+                    <AppLoader />
+                </div>
+            )}
+
+            <AppLayoutTemplate breadcrumbs={breadcrumbs} pageDetails={pageDetails} showQuickActions={showQuickActions} {...props}>
+                <div className={isLoading ? 'pointer-events-none opacity-50' : ''}>{children}</div>
+            </AppLayoutTemplate>
+        </>
+    );
+}
